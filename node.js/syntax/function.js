685034421e669d7c0fb3f1a85beb4bb5400a5687
HHH_day <you@example.com>
2022-08-26 07:22:34 +0900

    function

---
+++ node.js/syntax/function.js
@@ -0,0 +1,28 @@
+function f123(){
+    console.log(1);
+    console.log(2);
+    console.log(3);
+    console.log(4);
+}
+
+f123();
+console.log('A');
+console.log('B');
+console.log('C');
+f123();
+console.log('D');
+console.log('E');
+console.log('F');
+console.log('G');
+console.log('H');
+f123();
+console.log('I');
+console.log('J');
+console.log('K');
+console.log('L');
+console.log('M');
+f123();
+console.log('N');
+console.log('O');
+console.log('p');
+f123();
