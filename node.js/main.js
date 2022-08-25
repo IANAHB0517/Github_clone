8d0d4fd035ac3cfa92cfd4e2d1d2d04d1187353e
HHH_day <you@example.com>
2022-08-26 07:16:42 +0900

    autoadd

--- node.js/main.js
+++ node.js/main.js
@@ -10,61 +10,82 @@ var app = http.createServer(function(request,response){
 
     if(pathname ==='/'){ 
       if(queryData.id === undefined){
-        
-        
-          var title = 'welcome';
-          var description = 'Hello, Node.Js';
-          var template = `
-        <!doctype html>
-        <html>
+
+      fs.readdir('./data', function(error, filelist){
+      var title = 'welcome';
+      var description = 'Hello, Node.Js';
+      var list = '<ul>';
+      var i =0;
+
+      while(i < filelist.length){
+        list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+        i++;
+      }
+
+      list = list + '</ul>';
+
+
+      var template = `
+      <!doctype html>
+      <html>
         <head>
           <title>WEB1 - ${title}</title>
           <meta charset="utf-8">
         </head>
         <body>
           <h1><a href="/">WEB</a></h1>
-          <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=Javascript">JavaScript</a></li>
-          </ul>
+          ${list}
           <h2>${title}</h2>
           <p>${description}</p>
         </body>
-        </html>
+      </html>
     
         `;
         response.end(template);
         response.writeHead(200);
         // console.log(queryData.id);
+        })
+        
+        
         
       } else {
-        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-          var title = queryData.id;
-          var template = `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=Javascript">JavaScript</a></li>
-          </ul>
-          <h2>${title}</h2>
-          <p>${description}</p>
-        </body>
-        </html>
+        fs.readdir('./data', function(error, filelist){
+          var title = 'welcome';
+          var description = 'Hello, Node.Js';
+          var list = '<ul>';
+          var i =0;
+
+          while(i < filelist.length){
+            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+            i++;
+          }
     
-        `;
+          list = list + '</ul>';
+    
+
+          fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+            var title = queryData.id;
+            var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            ${list}
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+      
+          `;
         response.end(template);
         response.writeHead(200);
         // console.log(queryData.id);
         });
+      });
       }
       
     } else {
