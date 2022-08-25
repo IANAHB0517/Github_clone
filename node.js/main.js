3ecc53c4052ccb7b3ed2de1fc0237fc0ea587b5c
HHH_day <you@example.com>
2022-08-25 14:45:45 +0900

    404

--- node.js/main.js
+++ node.js/main.js
@@ -5,43 +5,42 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url,true).query;
+    var pathname = url.parse(_url, true).pathname;
     var title = queryData.id;
 
+    if(pathname ==='/'){
+      fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+        var template = `
+      <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        <ul>
+          <li><a href="/?id=HTML">HTML</a></li>
+          <li><a href="/?id=CSS">CSS</a></li>
+          <li><a href="/?id=Javascript">JavaScript</a></li>
+        </ul>
+        <h2>${title}</h2>
+        <p>${description}</p>
+      </body>
+      </html>
+  
+      `;
+      response.end(template);
+      response.writeHead(200);
+      // console.log(queryData.id);
+      })
+    } else {
+      response.writeHead(404);
+      response.end('Not found');      
+    }
+   
+   
     
-    if(_url == '/'){
-      title='WELCOME';
-    }
-
-    if(_url == '/favicon.ico'){
-      return response.writeHead(404);
-        // response.end();
-        
-    }
-    response.writeHead(200);
-    fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-      var template = `
-    <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      <ol>
-        <li><a href="/?id=HTML">HTML</a></li>
-        <li><a href="/?id=CSS">CSS</a></li>
-        <li><a href="/?id=Javascript">JavaScript</a></li>
-      </ol>
-      <h2>${title}</h2>
-      <p>${description}</p>
-    </body>
-    </html>
-
-    `;
-    response.end(template);
-    // console.log(queryData.id);
-    })
     
 
     
