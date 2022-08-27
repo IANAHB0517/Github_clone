300d30269c8eac03aad3d78111a29cb655d8fe55
IANAHB0517 <fromtheedge0517@gmail.com>
2022-08-27 16:29:39 +0900

    pm2

--- node.js/main.js
+++ node.js/main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
 function templateHTML(title, list, body){
   return `
@@ -13,6 +14,7 @@ function templateHTML(title, list, body){
     <body>
       <h1><a href="/">WEB</a></h1>
       ${list}
+      <a href="create">Create</a>
       ${body}
     </body>
   </html>`
@@ -36,8 +38,7 @@ var app = http.createServer(function(request,response){
   var queryData = url.parse(_url,true).query;
   var pathname = url.parse(_url, true).pathname;
     
-
-  if(pathname ==='/'){ 
+ if(pathname ==='/'){ 
     if(queryData.id === undefined){
       fs.readdir('./data', function(error, filelist){
         var title = 'welcome';
@@ -46,7 +47,7 @@ var app = http.createServer(function(request,response){
         var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
         response.end(template);
         response.writeHead(200);
-        })
+        });
       }
     else {
       fs.readdir('./data', function(error, filelist){
@@ -59,12 +60,45 @@ var app = http.createServer(function(request,response){
           });
         });
       }
-      
-    } else {
-      response.writeHead(404);
-      response.end('Not found');      
+  } else if (pathname === '/create'){
+      fs.readdir('./data', function(error, filelist){
+        var title = 'WEB - Create';
+        var list = templatelist(filelist);
+        var template = templateHTML(title, list, `
+        <form action="http://localhost:3000/create_process" method="post">
+          <p ><input type="text" name="title" placeholder="Title"></p>
+          <p>
+            <textarea name="description" placeholder="description"></textarea>
+          </p>
+          <p>
+            <input type="submit">
+          </p>
+        </form>
+        `);
+        response.writeHead(200);
+        response.end(template);
+        });
+  } else if (pathname === '/create_process'){
+    var body ='';
+    request.on('data',function(data){
+      body= body + data;
+    });
+    request.on('end', function(){
+      var post = qs.parse(body);
+      var title = post.title;
+      var description = post.description;
+      console.log(post);
+    });
+    response.writeHead(200);
+    response.end('success');
+
+  } else {
+
+    response.writeHead(404);
+    response.end('Not found');   
+
     }
    
 
 });
-app.listen(3000);
+app.listen(3000)
