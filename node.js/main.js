ccfbe67316a5dd2d89df3374af5eaec9d28513e1
IANAHB0517 <fromtheedge0517@gmail.com>
2022-08-30 17:15:30 +0900

    redirect

--- node.js/main.js
+++ node.js/main.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-var qs = require('querystring');
+var qust = require('querystring');
 
 function templateHTML(title, list, body){
   return `
@@ -49,6 +49,7 @@ var app = http.createServer(function(request,response){
         response.writeHead(200);
         });
       }
+      
     else {
       fs.readdir('./data', function(error, filelist){
         fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
@@ -60,6 +61,7 @@ var app = http.createServer(function(request,response){
           });
         });
       }
+
   } else if (pathname === '/create'){
       fs.readdir('./data', function(error, filelist){
         var title = 'WEB - Create';
@@ -78,19 +80,23 @@ var app = http.createServer(function(request,response){
         response.writeHead(200);
         response.end(template);
         });
+
   } else if (pathname === '/create_process'){
     var body ='';
     request.on('data',function(data){
       body= body + data;
     });
     request.on('end', function(){
-      var post = qs.parse(body);
+      var post = qust.parse(body);
       var title = post.title;
       var description = post.description;
-      console.log(post);
+      fs.writeFile(`data/${title}`,description, `utf8`,function(err){
+        response.writeHead(302, {Location:`/?id=${title}`});
+        response.end('success');
+      })
+      
     });
-    response.writeHead(200);
-    response.end('success');
+    
 
   } else {
 
