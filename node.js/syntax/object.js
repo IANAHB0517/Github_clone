64edf25b3d4cb6f02fa1af7f9da1006946067753
IANAHB0517 <fromtheedge0517@gmail.com>
2022-09-02 00:24:12 +0900

    for,while

--- node.js/syntax/object.js
+++ node.js/syntax/object.js
@@ -1,5 +1,11 @@
 var members = ['FTE','La.J','Dyangjin'];
-console.log(members[1]);
+console.log(members[0]);
+
+var i =0;
+while(i < members.length){
+    console.log(`loop` , members[i]);
+    i++
+}
 
 var roles = {
     'programmer' : 'FTE',
@@ -7,3 +13,7 @@ var roles = {
     'husband' : 'Dyangjin',
 }
 console.log(roles.musician);
+
+for(var n in roles){
+    console.log(`object =>` , n , 'value =>' , roles[n]);
+}
