d7e40adf5cddac6599d28ccd9b59f6aa54adada6
IANAHB0517 <fromtheedge0517@gmail.com>
2022-09-02 00:18:27 +0900

    object

---
+++ node.js/syntax/object.js
@@ -0,0 +1,9 @@
+var members = ['FTE','La.J','Dyangjin'];
+console.log(members[1]);
+
+var roles = {
+    'programmer' : 'FTE',
+    'musician' : 'La.J',
+    'husband' : 'Dyangjin',
+}
+console.log(roles.musician);
