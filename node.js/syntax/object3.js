01eaefcbf476bafe9dd7b59b1395d939da2fb0c6
IANAHB0517 <fromtheedge0517@gmail.com>
2022-09-05 13:22:08 +0900

    object3

---
+++ node.js/syntax/object3.js
@@ -0,0 +1,20 @@
+// var v1 = 'v1';
+// // 10000 code
+// v1 = 'FTE';
+// var v2 = 'v2';
+
+var o = {
+    v1:'v1',
+    v2:'v2'
+}
+
+function f1(){
+    console.log(o.v1);
+}
+
+function f2(){
+    console.log(o.v2);
+}
+
+f1();
+f2();
